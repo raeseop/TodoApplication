7913e828adc6becab557de01965fe07b0e6065c2
raeseopHong <hrs2289@naver.com>
2023-08-07 16:13:13 +0900

    Todo_application

---
+++ src/main/java/com/example/todoapplication/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.todoapplication.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+    @GetMapping
+    public String getTodo(){
+        return "To-do Application!";
+    }
+}
